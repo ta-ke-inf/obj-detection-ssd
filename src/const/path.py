8e3174737e26aa5edee7b8bf638e21fb1f60284a
takeguchi0109 <hirotake.stu@gmail.com>
2023-02-19 21:03:58 +0900

    Add const, dataset

---
+++ src/const/path.py
@@ -0,0 +1,4 @@
+from os.path import abspath, dirname, join
+
+PROJECT_ROOT = join(abspath(dirname(__file__)), "..")
+DATA_PATH = join(PROJECT_ROOT, "../downloads/2_objectdetection/data/VOCdevkit/VOC2012/")

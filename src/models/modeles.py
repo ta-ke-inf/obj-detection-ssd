e536ae291ac24f4fb12fa7d75d48a4128d439d22
takeguchi0109 <hirotake.stu@gmail.com>
2023-02-24 02:30:51 +0900

    Update modules Add make_loc_conf

--- src/models/modeles.py
+++ src/models/modeles.py
@@ -27,6 +27,7 @@ def make_vgg():
 
     return nn.ModuleList(layers)
 
+
 def make_extras():
     layers = []
     in_channels = 1024
@@ -45,9 +46,54 @@ def make_extras():
     return nn.ModuleList(layers)
 
 
+def make_loc_conf(num_classes=21, bbox_aspect_num=[4, 6, 6, 6, 4, 4]):
+    """
+    Args
+        num_classes: int
+            クラスの数 = 21
+
+        bbox_aspect_num: list
+            各要素は DBox の個数
+    """
+
+    loc_layers = []
+    conf_layers = []
+
+    # * 4 はオフセットの情報（4変数）
+
+    # vgg: source1 に対する conv
+    loc_layers += [nn.Conv2d(512, bbox_aspect_num[0] * 4, kernel_size=3, padding=1)]
+    conf_layers += [nn.Conv2d(512, bbox_aspect_num[0] * num_classes, kernel_size=3, padding=1)]
+
+    # vgg: source2 に対する conv
+    loc_layers += [nn.Conv2d(1024, bbox_aspect_num[1] * 4, kernel_size=3, padding=1)]
+    conf_layers += [nn.Conv2d(1024, bbox_aspect_num[1] * num_classes, kernel_size=3, padding=1)]
+
+    # extras: source3 に対する conv
+    loc_layers += [nn.Conv2d(512, bbox_aspect_num[2] * 4, kernel_size=3, padding=1)]
+    conf_layers += [nn.Conv2d(512, bbox_aspect_num[2] * num_classes, kernel_size=3, padding=1)]
+
+    # extras: source4 に対する conv
+    loc_layers += [nn.Conv2d(256, bbox_aspect_num[3] * 4, kernel_size=3, padding=1)]
+    conf_layers += [nn.Conv2d(256, bbox_aspect_num[3] * num_classes, kernel_size=3, padding=1)]
+
+    # extras: source5 に対する conv
+    loc_layers += [nn.Conv2d(256, bbox_aspect_num[4] * 4, kernel_size=3, padding=1)]
+    conf_layers += [nn.Conv2d(256, bbox_aspect_num[4] * num_classes, kernel_size=3, padding=1)]
+
+    # extras: source6 に対する conv
+    loc_layers += [nn.Conv2d(256, bbox_aspect_num[5] * 4, kernel_size=3, padding=1)]
+    conf_layers += [nn.Conv2d(256, bbox_aspect_num[5] * num_classes, kernel_size=3, padding=1)]
+
+    return nn.ModuleList(loc_layers), nn.ModuleList(conf_layers)
+
 
 if __name__ == "__main__":
     vgg_test = make_vgg()
     extras_test = make_extras()
     print(vgg_test)
     print(extras_test)
+
+    loc_test, conf_test = make_loc_conf()
+    print(loc_test)
+    print(conf_test)

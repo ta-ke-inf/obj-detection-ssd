3fa484fb6a9bd8223084a46dd0f1de76dc0f0622
takeguchi0109 <hirotake.stu@gmail.com>
2023-02-24 01:29:45 +0900

    Update modeles

--- src/models/modeles.py
+++ src/models/modeles.py
@@ -27,6 +27,27 @@ def make_vgg():
 
     return nn.ModuleList(layers)
 
+def make_extras():
+    layers = []
+    in_channels = 1024
+
+    cfg = [256, 512, 128, 256, 128, 256, 128, 256]
+
+    layers += [nn.Conv2d(in_channels, cfg[0], kernel_size=(1))]
+    layers += [nn.Conv2d(cfg[0], cfg[1], kernel_size=(3), stride=2, padding=1)]
+    layers += [nn.Conv2d(cfg[1], cfg[2], kernel_size=(1))]
+    layers += [nn.Conv2d(cfg[2], cfg[3], kernel_size=(3), stride=2, padding=1)]
+    layers += [nn.Conv2d(cfg[3], cfg[4], kernel_size=(1))]
+    layers += [nn.Conv2d(cfg[4], cfg[5], kernel_size=(3))]
+    layers += [nn.Conv2d(cfg[5], cfg[6], kernel_size=(1))]
+    layers += [nn.Conv2d(cfg[6], cfg[7], kernel_size=(3))]
+
+    return nn.ModuleList(layers)
+
+
+
 if __name__ == "__main__":
     vgg_test = make_vgg()
+    extras_test = make_extras()
     print(vgg_test)
+    print(extras_test)

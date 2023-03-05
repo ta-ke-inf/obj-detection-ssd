cd0fea1b0a4c454c7ce3b720c5a96fe1b948f8e4
takeguchi0109 <hirotake.stu@gmail.com>
2023-03-05 19:07:54 +0900

    Update box_utils

--- src/models/box_utils.py
+++ src/models/box_utils.py
@@ -64,14 +64,20 @@ def nm_suppression(boxes, scores, overlap=0.45, top_k=200):
 
     # numel: tensor の要素数. 200
     while idx.numel() > 0:
+        # conf が最大の index を取得する
         i = idx[-1]
+        # conf が最大の index を格納
+        keep[count] = i
+        count += 1
+        # 最大のconfを格納したので index を一つ減らす
+        idx = idx[:-1]
 
-
-
-
-
-
-
+        # keep に格納した 最大のconf のBBoxとそれ以外の idx のBBox の重なり度合いを計算指定していく
+        # 最大の conf の BBox 以外の BBox を取得
+        torch.index_select(x1, 0, idx, out=tmp_x1) # x1 のうち idx で指定された BBox のみ out に出力される
+        torch.index_select(y1, 0, idx, out=tmp_y1)
+        torch.index_select(x2, 0, idx, out=tmp_x2)
+        torch.index_select(y2, 0, idx, out=tmp_y2)
 
 
 

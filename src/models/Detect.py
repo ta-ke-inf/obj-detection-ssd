c063b353acc2b04519f1f670b8b5de28b8c557d9
takeguchi0109 <hirotake.stu@gmail.com>
2023-03-27 01:45:47 +0900

    Update Detect.py

--- src/models/Detect.py
+++ src/models/Detect.py
@@ -1,11 +1,18 @@
 #検出結果となるBBoxを抽出する
 
-import torch
+import torch.nn as nn
+from torch.autograd import Function
 
 
-class Detect(Funcrion):
-    def __init__(self):
+class Detect(Function):
+    def __init__(self, conf_thresh=0.01, top_k=200, nms_thresh=0.45):
+        self.softmax = nn.Softmax(dim=-1) # conf のためのsoftmaxでクラスごとに正規化
+        self.conf_thresh = conf_thresh # nm_supression の処理を軽くするため conf の値が 0.01 よりも大きい DBox のみ扱う
+        self.top_k = top_k # nm_supression 内の top_k
+        self.nms_thresh = nms_thresh
+
+    def forward(self, loc_data, conf_data, dbox_list):
         pass
 
-    def forward(self):
-        pass
+if __name__ == "__main__":
+    pass

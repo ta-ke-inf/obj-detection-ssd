b7ad7e8455ead7a0d90008663d370a5e18e33e87
takeguchi0109 <hirotake.stu@gmail.com>
2023-03-31 13:38:01 +0900

    Update loss.py

--- src/utils/loss.py
+++ src/utils/loss.py
@@ -1,3 +1,4 @@
+import torch
 import torch.nn as nn
 
 
@@ -26,3 +27,35 @@ class MultiBoxLoss(nn.Module):
         num_batch = loc_data.size(0)
         num_dbox = loc_data.size(1)
         num_classes = conf_data.size(2)
+
+        # 各 DBox に対して一番近い BBox のラベルを格納 ( background を 0 )
+        conf_t_label = torch.LongTensor(num_batch, num_dbox).to(self.device)
+        # 各 DBox に対して一番近い BBox の補正情報を格納
+        loc_t = torch.LongTensor(num_batch, num_dbox, 4).to(self.device)
+
+        for idx in range(num_batch):
+            # 正解 BBox の位置情報
+            truths = targets[idx][:, -1].to(self.device)
+            # 正解 BBox のラベル
+            labels = targets[idx][:, :-1].to(self.device)
+
+            variance = [0.1, 0.2]
+            # loc_t と conf_t_label が上書きされる
+            # loc_t : [num_batch, 8732, 4]
+            # conf_t_label : [num_batch, 8732]
+            match(self.jaccard_thresh, truths, dbox, variance, labels, loc_t, conf_t_label, idx)
+
+        #===========
+        # loss_l の計算
+        #===========
+
+        # Positive DBox のマスクを取得
+        pos_mask = conf_t_label > 0 # [num_batch, 8732]
+        pos_idx = pos_mask.unsqueeze(pos_mask.dim()).expand_as(loc_data) # mask を適用させるために [num_batch, 8732] -> [num_batch, 8732, 4] に変形
+
+        # mask を適用すると 1次元に圧縮される仕様なので, view で 次元を増やしている
+        loc_t = loc_t[pos_idx].view(-1, 4) # 教師データ(正解BBoxに対するオフセット) : [物体を検出したBBoxの数(全ミニバッチの合計), 4]
+        loc_p = loc_data[pos_idx].view(-1, 4) # 予測データ(予測BBoxに対するオフセット) : [物体を検出したBBoxの数(全ミニバッチの合計), 4]
+
+        # Positive DBox に対する損失を計算
+        loss_l = F.smooth_l1_loss(loc_p, loc_t, reduction='sum')

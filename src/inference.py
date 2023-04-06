5c5dc640264293719f3a4ffab7168bd8ea07d78c
ta-ke-inf <hirotake.stu@gmail.com>
2023-04-06 13:28:42 +0900

    Add inference function

--- src/inference.py
+++ src/inference.py
@@ -2,6 +2,7 @@ from models.SSD import SSD
 import torch
 import cv2
 import matplotlib.pyplot as plt
+from utils.preprocess.DataTransform import DataTransform
 
 def read_img(img_file_path) -> tuple:
     """
@@ -22,6 +23,18 @@ def show_img(img) -> None:
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     plt.imshow(img)
     plt.show()
+
+def inference(img):
+    """
+    Args:
+        img (str): opencv image [height, width, channels]
+    Returns:
+        detections (torch.Tensor): [num_batch, num_classes, num_detections, 5]
+    """
+    net.eval()
+    x = img.unsqueeze(0) # [1, 3, 300, 300]
+    detections = net(x)
+    return detections
 
 
 if __name__ == "__main__":
@@ -44,8 +57,18 @@ if __name__ == "__main__":
 
     net = SSD(phase="inference", cfg=ssd_cfg)
 
-    net_weights = torch.load("./weights/ssd300_mAP_77.43_v2.pth", map_location={'cuda:0': 'cpu'})
+    net_weights = torch.load("../pytorch_advanced/2_objectdetection/weights/ssd300_mAP_77.43_v2.pth", map_location={'cuda:0': 'cpu'})
     net.load_state_dict(net_weights)
 
     img, height, width, channels = read_img("../pytorch_advanced/2_objectdetection/data/cowboy-757575_640.jpg")
-    show_img(img)
+    #show_img(img)
+
+    color_mean = (104, 117, 123) # BGR mean
+    input_size = 300 # resize to 300x300
+    transform = DataTransform(input_size, color_mean)
+    phase = "val"
+    img_transformed, boxes, labels = transform(img, phase, "", "")
+    img = torch.from_numpy(img_transformed[:, :, (2, 1, 0)]).permute(2, 0, 1)
+
+    detections = inference(img)
+    print(detections.shape)
